15aacb125c89e2faaca2e572f1ac8e4e20fddf7a
Jevford <jevfordbarro@gmail.com>
2020-05-07 14:45:05 -0700

    Styled and Added Update Button to Prefs Page

--- screens/Preferences.js
+++ screens/Preferences.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View, Text, StyleSheet, SafeAreaView, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Platform} from 'react-native';
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
 
 import Header from '../components/Header';
@@ -28,11 +28,10 @@ export default class Preferences extends Component {
         return (
             <View style={styles.container}>
                 <Header title='Preferences'/>
-                <View style={styles.container}>
-                    <Text>Preferences Screen</Text>
-                    <Text>Cost: {this.state.costValue}</Text>
-                    <Text>Environment: {this.state.envValue}</Text>
-                    <Text>Society: {this.state.socValue}</Text>
+                <View style={styles.prefContainer}>
+                    <Text style={styles.costText}>Cost: {this.state.costValue}</Text>
+                    <Text style={styles.envText}>Environment: {this.state.envValue}</Text>
+                    <Text style={styles.socText}>Society: {this.state.socValue}</Text>
                     <MultiSlider
                         values={[30,70]}
                         max={100}
@@ -43,15 +42,53 @@ export default class Preferences extends Component {
                         minMarkerOverlapDistance={10}
                     />
                 </View>
+                <TouchableOpacity>
+                    <View>
+                        <Text style={styles.saveBtn}> Save Preferences </Text>
+                    </View>
+                </TouchableOpacity>
             </View> 
         );
     }
 }
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    prefContainer: {
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        fontWeight: "bold",
+        top: 20
+    },
+    costText: {
+        fontSize: 20,
+        color: "#42b6f5",
+        fontWeight: "bold",
+        paddingBottom: 10
+    },  
+    envText: {
+        fontSize: 20,
+        color: "#65CB89",
+        fontWeight: "bold",
+        paddingBottom: 10
+    },  
+    socText: {
+        fontSize: 20,
+        color: "#d4c559",
+        paddingBottom: 10
+    },  
+    saveBtn: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        bottom: 20,
+        fontSize: 24,
+        fontWeight: "bold",
+        color: '#65CB89'
     }
 })
