b42ff7b25b57362658097abb2cc1a106ded32a28
DBelen <dbelen@uci.edu>
2020-07-07 18:28:51 -0700

    Update API URL

--- components/axiosInstance.js
+++ components/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Keep this config private as this is the direct link to the CalPlug database API
 const instance = axios.create({
     // The base url must use the https protocal to allow secure connections to CalPlug's servers
-    baseURL: 'https://cpmqtt1.calit2.uci.edu/'
+    baseURL: 'https://cpmqtt1.calit2.uci.edu/api'
 });
 
 export default instance;
